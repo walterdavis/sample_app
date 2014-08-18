8cb091215bd374859ae9f4fc0d975988182c1b11
Walter Davis <waldavis@mail.med.upenn.edu>
2014-08-18 15:13:27 -0400

    finishing up layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+  match '/signup', to: "users#new", via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
