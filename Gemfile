b4e97d26a0dd8741e7822b6e0b9a441cef52fd97
Walter Davis <waldavis@mail.med.upenn.edu>
2014-08-18 12:25:56 -0400

    tests are passing, added guard

--- Gemfile
+++ Gemfile
@@ -5,8 +5,9 @@ ruby '2.0.0'
 gem 'rails', '4.0.8'
 
 group :development, :test do
-  gem 'sqlite3', '1.3.8'
+  gem 'pg', '0.15.1'
   gem 'rspec-rails', '2.13.1'
+  gem 'guard-rspec', '2.5.0'
 end
 
 group :test do
@@ -26,6 +27,5 @@ group :doc do
 end
 
 group :production do
-  gem 'pg', '0.15.1'
   gem 'rails_12factor', '0.0.2'
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -39,6 +39,19 @@ describe "Static Pages" do
     end
     
   end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
+    end
+    
+  end
   
   
 end
