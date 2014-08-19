a55934ffb8233dc662334a89bd091179eb43c8b7
Walter Davis <waldavis@mail.med.upenn.edu>
2014-08-19 11:13:31 -0400

    add ssl in production

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.force_ssl = true
     
   end
 end
