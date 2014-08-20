10b10241f92a17f5098ef56254d906bcb952157b
Walter Davis <waldavis@mail.med.upenn.edu>
2014-08-20 12:42:20 -0400

    added edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,11 +19,16 @@ class UsersController < ApplicationController
   end
   
   def edit
-    
+    @user = User.find(params[:id])
   end
   
   def update
-    
+    @user = User.find(params[:id])
+    if @user.update_attributes( user_params )
+      redirect_to @user
+    else
+      render :edit
+    end
   end
   
   private
