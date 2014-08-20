98b690dba2af2e74c6f3e4a30a042e95812a931c
Walter Davis <waldavis@mail.med.upenn.edu>
2014-08-20 08:03:07 -0400

    finished login logout system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,28 @@
 class User < ActiveRecord::Base
+  has_secure_password
   validates :name, :email, presence: true
   validates :name, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                      uniqueness: { case_sensitive: false }
-  before_save { email.downcase! }
-  has_secure_password
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+
+  before_save { email.downcase! }
+  before_create :create_remember_token
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.digest(User.new_remember_token)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users
   match '/signup', to: "users#new", via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
