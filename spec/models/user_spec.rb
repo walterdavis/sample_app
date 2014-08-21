60fda6a5563289fa93c71fb7f18768df84723de4
Walter Davis <waldavis@mail.med.upenn.edu>
2014-08-21 12:02:34 -0400

    added correct mixed feed to user page

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -130,9 +130,11 @@ describe User do
   describe "micropost associations" do
 
     before { @user.save }
+    
     let!(:older_micropost) do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
     end
+    
     let!(:newer_micropost) do
       FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
     end
@@ -140,6 +142,7 @@ describe User do
     it "should have the right microposts in the right order" do
       expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
     end
+    
     it "should destroy associated microposts" do
       microposts = @user.microposts.to_a
       @user.destroy
@@ -148,17 +151,29 @@ describe User do
         expect(Micropost.where(id: micropost.id)).to be_empty
       end
     end
+    
     describe "status" do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
       end
 
       its(:feed) { should include(newer_micropost) }
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
     end
-    
-    
+
+
 
   end
 
